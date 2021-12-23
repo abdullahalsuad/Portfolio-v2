4f6a483d1ed5a9107a1a3cc0777aec0b1aedeab9
suad <suadabdullahal@gmail.com>
2021-12-24 02:11:14 +0600

    project  page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { About } from "./Component/about/About";
 import Intro from "./Component/intro/Intro";
+import ProductList from "./Component/productLIst/ProductList";
 
 function App() {
   return (
     <div >
      <Intro />
      <About />
+     <ProductList />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://forum.keyboardmaestro.com/uploads/default/original/3X/1/7/1721aed5d4fab0ce5d6af1301685a3ce0e16dd5e.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "../src/img/abdullahalsuad.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
+
+
+//   export const products = [
+//     {
+//       id: 1,
+//       img: "./img/abdullahalsuad.png",
+//       link: "http://lama.dev",
+//     },
+//     {
+//       id: 2,
+//       img: "./img/cryptodaddy.png",
+//       link: "http://lama.dev",
+//     },
+//     {
+//       id: 3,
+//       img: "",
+//       link: "./img/jakanaka-pizzabd.png",
+//     },
+//     {
+//       id: 4,
+//       img: "",
+//       link: "./img/tesla-home.png",
+//     },
+//     {
+//       id: 5,
+//       img: "",
+//       link: "./img/pokemon-evolution.png",
+//     },
+//     {
+//       id: 6,
+//       img: "",
+//       link: "./img/cc-tracker.png",
+//     },
+//   ];
