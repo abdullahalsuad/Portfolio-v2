bd27a723437079198d9cbfda977a09ead6e405d3
suad <suadabdullahal@gmail.com>
2021-12-24 21:39:29 +0600

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useContext } from "react";
 import { About } from "./Component/about/About";
 import { Contact } from "./Component/contact/Contact";
 import Intro from "./Component/intro/Intro";
 import ProductList from "./Component/productLIst/ProductList";
+import Toggle from "./Component/toggle/Toggle";
+import { ThemeContext } from "./context";
 
 function App() {
+
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode
+
   return (
-    <div >
+    <div style={{
+      backgroundColor: darkMode ? "#222" : "white",
+      color: darkMode && "white",
+    }}>
+      <Toggle />
      <Intro />
      <About />
      <ProductList />
